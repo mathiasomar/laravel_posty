d8cdd1234e809deb099f925c54dbd36be68ee499
mathiasomar <ommykmathy@gmail.com>
2024-11-01 21:13:37 +0300

    post posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view("posts.index");
+        $posts = Post::paginate(10);
+        return view("posts.index", [
+            'posts' => $posts
+        ]);
     }
 
     public function store(Request $request) {
-        dd("ok");
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
+
 }
