dae7111860fd99a64c1f04482931edf94021f54f
mathiasomar <ommykmathy@gmail.com>
2024-11-01 11:06:07 +0300

    creating auth middleware

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/home", function () {
+Route::get("/", function () {
     return view("home");
 })->name('home');
 
 Route::get("/dashboard", [DashboardController::class, 'index'])->name('dashboard');
+// ->middleware('auth');
 
 Route::post("/logout", [LogoutController::class, 'store'])->name('logout');
 
