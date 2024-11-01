c36ad04a626db34c9e19436fc59e8d1dab64e97b
mathiasomar <ommykmathy@gmail.com>
2024-11-01 10:44:01 +0300

    laravel tutorial initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get("/home", function () {
+    return view("home");
+})->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/dashboard", [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post("/logout", [LogoutController::class, 'store'])->name('logout');
+
+Route::get("/login", [LoginController::class, 'index'])->name('login');
+Route::post("/login", [LoginController::class, 'store']);
+
+Route::get("/register", [RegisterController::class, 'index'])->name('register');
+Route::post("/register", [RegisterController::class, 'store']);
+
+Route::get('/posts', function () {
+    return view('posts.index');
 });
