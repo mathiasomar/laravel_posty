e72ffee1bfdecad082417939b0d85334656db377
mathiasomar <ommykmathy@gmail.com>
2024-11-03 15:24:40 +0300

    eager loading

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,9 +30,25 @@
                 class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
             <p class="mb-2">{{ $post->body }}</p>
         </div>
+
+        <div class="flex items-center">
+            @if (!$post->likedBy(auth()->user()))
+            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                @csrf
+                <button type="submit" class="text-blue-500">Like</button>
+            </form>
+            @else
+            <form action="" method="post" class="mr-1">
+                @csrf
+                <button type="submit" class="text-blue-500">Unlike</button>
+            </form>
+            @endif
+
+            <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+        </div>
         @endforeach
 
-        {{ $posts->links() }}
+        {{ $posts->links('pagination::tailwind') }}
         @else
         <p>There are no posts</p>
         @endif
